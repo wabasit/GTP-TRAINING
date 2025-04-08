8af4f0b6f2dc97e10a73beeaac1bda25d630c347
wabasit <basit.wahab@amalitechtraining.org>
2025-04-08 10:55:46 +0000

    Saved extracted data into a json file

--- api/data_extraction.py
+++ api/data_extraction.py
@@ -1,33 +1,25 @@
-from functools import wraps
+import json
 import sys
 
 import requests
 import pandas as pd
 
-from config import TOKEN
-
-def memoize(func):
-    cache = {}
-
-    @wraps
-    def wrapper(*args, **kwargs):
-        key = str(args) + str(kwargs)
-
-        if key not in cache:
-            cache[key] = func(*args, **kwargs)
-            return cache[key]
+from config import TOKEN, API_KEY
     
-    return wrapper
-
-@memoize    
-def extract_data(self):
+def extract_data(self, save_file_path):
+    """
+    This function extracts, and dump movie data from the Movies API and dump into a json file.
+    Some specific movie ids were specified for extraction.
+    """
 
     movie_ids = [299534, 19995, 140607, 299536, 597, 135397,\
                     20818, 24428, 168259, 99861, 284054, 12445,181808,\
                     330457, 351286, 109445, 321612, 260513]
     
+    movie_data = []
+    
     for movie_id in movie_ids:
-        url = f"https://api.themoviedb.org/3/movie/{movie_id}"
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&append_to_response=credits"
 
         headers = {
             "accept": "application/json",
@@ -35,10 +27,18 @@ def extract_data(self):
         }
 
         response = requests.get(url, headers=headers)
-        data = response.json()
+        
+        if response.status_code == 200:
+            movies = response.json()
+            movie_data.append(movies)
+        else:
+            return f'Error Code: {response.status_code};\nMsg: Movie with {movie_id} was not found!'
+        movie_df = pd.DataFrame(movie_data)
 
-        df = pd.json_normalize(data)
-        return df
+        with open(save_file_path, 'w') as f:
+            data = json.dump(movie_df, f, ensure_ascii=False, indent=4)
+
+    return data
 
 
 
