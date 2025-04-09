5898e0ac33bf51da2180cde47e99c0b3ba3e9e29
wabasit <basit.wahab@amalitechtraining.org>
2025-04-09 04:52:54 +0000

    Altered a few columns and dropped status

--- data_preprocessing/handling_missing_values.py
+++ data_preprocessing/handling_missing_values.py
@@ -1,13 +1,10 @@
 import pandas as pd
 import numpy as np
 
-from data_preprocessing.data_clean import clean
-
-data = clean()
 
 def handle_missing_values(data):
     col_convert = ['budget', 'id', 'popularity']
-    converted_to_num = [pd.to_numeric(data[d]) for d in col_convert]
+    data[col_convert] = [pd.to_numeric(data[d]) for d in col_convert]
     data['release_date'] = pd.to_datetime(data['release_date'])
 
 def convert_values(data):
@@ -18,5 +15,8 @@ def convert_values(data):
     data[million_dollars] = data[million_dollars] / 1000000
     data[placeholder_replace] = data[placeholder_replace].replace('No Data', np.nan)
     data = data.drop_duplicates()
+    data.loc[data['vote_counts']==0, 'vote_average'] = np.nan
+    data['Released'] = data.loc[data['status']=='Released', 'status']
+    data.drop(columns='status', axis=1, inplace=True)
 
     
