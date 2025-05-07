7456e27438d69c606e3339bbc8cb728a67dead73
wabasit <basit.wahab@amalitechtraining.org>
2025-05-07 16:46:45 +0000

    Update heartbeats table schema to enforce timestamp as TIMESTAMPTZ and ensure heart_rate is not nullable

--- Kafka_stream/schema.sql
+++ Kafka_stream/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS heartbeats (
     id SERIAL PRIMARY KEY,
     customer_id INTEGER,
-    timestamp BIGINT,
-    heart_rate INTEGER,
+    timestamp TIMESTAMPTZ NOT NULL,
+    heart_rate INTEGER NOT NULL,
     anomaly BOOLEAN DEFAULT FALSE
 );
 CREATE INDEX idx_timestamp ON heartbeats (timestamp);
